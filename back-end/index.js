ed29b38a7dff2d93cfb8d1452da9bd12e8feed05
Juan Carlos <juancarlosti@rede.ulbra.br>
2025-01-02 16:23:32 -0300

    concluido - parte 1,2,3 e 4

--- back-end/index.js
+++ back-end/index.js
@@ -19,7 +19,7 @@ genero.map(item => {
     console.log(item);
 })
 
-// Get array de todos os gêneros
+// GET
 app.get('/gender', (req, res) => {
     res.status(200).json({
         message: "GET: 200",
@@ -27,24 +27,25 @@ app.get('/gender', (req, res) => {
     });
 });
 
-
 // POST
 app.post('/gender', (req, res) => {
     // O post verificará se o id e o nome já existem no arquivo JSON
     if (req.body.nome === "" || req.body.id === "") {
-        throw new Error(res.status(409).json({
+        res.status(409).json({
             message: 'Erro - Preencha os campos obrigatórios - nome e Id'
-        }))
+        })
     }
 
     // Salva o valor do ID em uma variável
     const validationId = parseInt(req.body.id);
 
-    console.log(genero.some(item => item && item.id == validationId));
+    // console.log(!genero.some(item => item && item.id == validationId));
 
     try {
+        // Verificar se existe algum item com o id igual o input
         if (!genero.some(item => item && item.id == validationId)) {
             console.log('Entrei no if')
+            console.log()
             // Verificar se existe um gênero com o nome igual o da requisição;
             const nomeExistente = genero.some(item => item && item.nome === req.body.nome);
 
@@ -57,10 +58,10 @@ app.post('/gender', (req, res) => {
             console.log('Passou na validação');
 
             // Escrever JSON utilizando o FS
-            genero[validationId] = req.body;
+            genero.push(req.body);
             res.status(200).json({
                 message: 'Dados foram registrados!',
-                data: genero[validationId]
+                data: genero[genero.length - 1]
             });
             JSON.parse(fs.writeFileSync('./src/json/data.json', JSON.stringify(genero, null, 2), 'utf-8'));
             return;
@@ -69,8 +70,7 @@ app.post('/gender', (req, res) => {
             // console.log(req.body);
         } else {
             return res.status(404).json({
-                message: 'ID excedeu o tamanho da lista ou é inválido.',
-                data: []
+                message: 'ID excedeu o tamanho da lista ou é inválido.'
             });
         }
     } catch (err) {
@@ -78,22 +78,23 @@ app.post('/gender', (req, res) => {
         if (!res.headersSent) {
             res.status(500).json({ message: "Erro no cabecalho " , error: err.message });
         }
-        
+        res.status(500).json({
+            message: 'Aconteceu um erro no servidor!',
+        });
         
     }
 
 })
 
 // PUT
-// Quando o put ser executado, vai falhar porque o get está usando um nome que vai ser alterado, logo após a alteração, o nome atual deixa de ser válido pois não existe.
 app.put('/gender/:id', (req, res) => {
     // Salva o valor do ID em uma variável
     const validationId = parseInt(req.params.id);
     // Verifica se o nome foi preenchido
     if (!req.body.nome) {
-        throw new Error(res.status(409).json({
+        res.status(409).json({
             message: 'Erro - Nome Obrigatório.'
-        }))
+        })
     }
 
     try {
@@ -104,82 +105,89 @@ app.put('/gender/:id', (req, res) => {
         // ID NUNCA MUDA, somente nome e descrição.
 
         // Validação da ID
-        if (validationId <= genero.genero.length - 1) {
+        // Verificar se existe algum item com o id igual o input
+        if (!genero.some(item => item && item.id == validationId)) {
+            res.status(404).json({
+                message: 'ID excedeu o tamanho da lista ou é inválido.',
+                data: []
+            });
+        } else {
             console.log('Validação: ', req.params.id, '\nReq:', req.body);
 
             // Verificar se existe um gênero com o nome igual o da requisição;
-            genero.genero.map(item => {
+            genero.map(item => {
                 if (req.body.nome == item.nome) {
-                    throw new Error(res.status(409).json({
+                    res.status(409).json({
                         message: 'Erro - Nome já existe no arquivo.'
-                    }));
+                    });
                 } else {
                     console.log('Passou na verificação');
                 }
             });
 
             // Escrever JSON utilizando o FS
-            genero.genero[validationId] = req.body;
-            res.status(200).json({
-                message: 'Dados foram registrados!',
-                data: genero[validationId]
-            });
-            JSON.parse(fs.writeFileSync('./src/json/data.json', JSON.stringify(genero, null, 2), 'utf-8'));
-            return;
 
-        } else {
-            res.status(404).json({
-                message: 'ID excedeu o tamanho da lista ou é inválido.',
-                data: []
-            });
-            throw new Error('ID excedeu o tamanho da lista ou é inválido.');
+            const alterarId = genero.find(genero => genero.id == validationId);
+            if (alterarId){
+                alterarId.nome = req.body.nome
+                alterarId.descricao = req.body.descricao
+                res.status(200).json({
+                    message: 'Dados foram registrados!',
+                    data : genero[genero.length - 1]
+                });
+                JSON.parse(fs.writeFileSync('./src/json/data.json', JSON.stringify(genero, null, 2), 'utf-8'));
+                return;
+            }
         }
     } catch (err) {
+        console.error('Erro: ', err.message);
         if (!res.headersSent) {
-            res.status(400).json({ message: "Erro no cabecalho " + err.message });
+            res.status(400).json({ message: "Erro no envio do cabecalho " + err.message });
         }
         res.status(500).json({
             message: 'Aconteceu um erro no servidor!',
         });
-        console.error('Erro: ', err.message);
+        
     }
 })
 
 // DELETE
 app.delete('/gender/:id', (req, res) => {
-
-    // console.log('ID', req.params.id);
     if (!req.params.id) {
-        throw new Error(res.status(409).json({
+        res.status(409).json({
             message: 'Erro - Preencha os campos obrigatórios - Id'
-        }))
+        })
     }
 
     const validationId = parseInt(req.params.id);
-
     try {
-        if (validationId <= genero.genero.length - 1) {
-            console.log('Remoção de ID existente');
+        if (genero.some(item => item.id == validationId)) {
             // Achar o id - excluir o ID
+            const novoJson = genero.filter(item => item.id != validationId);
+            res.status(200).json({
+                message: `Removido - ID :${validationId}`,
+            })
+            genero.length = 0
+            genero.push(...novoJson);
+            console.log(genero);
 
-
-            console.log();
+            JSON.parse(fs.writeFileSync('./src/json/data.json', JSON.stringify(novoJson, null, 2), 'utf-8'));
+            return;
         } else {
             res.status(404).json({
                 message: 'ID inválido.',
                 data: []
             });
-            throw new Error('ID inválido.');
         }
 
     } catch (err) {
+        console.error('Erro: ', err.message);
         if (!res.headersSent) {
             res.status(400).json({ message: "Erro no cabecalho " + err.message });
         }
         res.status(500).json({
             message: 'Aconteceu um erro no servidor!',
         });
-        console.error('Erro: ', err.message);
     }
 })
 
